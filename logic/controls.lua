e02fdca28d7f02339fcb23f06c0c8347f282369e
cyberc001 <cyb3rc001@yandex.ru>
2024-04-30 11:08:17 +0300

    Fixed a bug that prevented units from being deselected.

--- logic/controls.lua
+++ logic/controls.lua
@@ -2,24 +2,27 @@ player_selected_objects = {}
 player_selected_objects.__mode = "k"
 
 function controls_tick()
-	local selected_something = false
 	local selection_query = controls.get_selection_query()
-	if selection_query ~= 0 then
-		for _,v in ipairs(game_object_arr) do
-			if gmath.hexahedron_is_selected(v.hitbox) then
-				if v.team == player_team then
-					if not selected_something then
-						player_selected_objects = {}
-						selected_something = true
-					end
-					player_selected_objects[v] = true
-				elseif selection_query == 2 and not v.team.allies[player_team] then -- check for an attack order
+	if selection_query == 0 then
+		return
+	end
+
+	local selected_something = false
+	for _,v in ipairs(game_object_arr) do
+		if gmath.hexahedron_is_selected(v.hitbox) then
+			if v.team == player_team then
+				if not selected_something then
+					player_selected_objects = {}
 					selected_something = true
-					for k in pairs(player_selected_objects) do
-						k:set_target(v)
-					end
-					break
 				end
+				player_selected_objects[v] = true
+				print("selected", v)
+			elseif selection_query == 2 and not v.team.allies[player_team] then -- check for an attack order
+				selected_something = true
+				for k in pairs(player_selected_objects) do
+					k:set_target(v)
+				end
+				break
 			end
 		end
 	end
@@ -31,5 +34,7 @@ function controls_tick()
 			k:set_goal(order_pos)
 			pointer_flash(vec3:new(order_pos.x, order_pos.y + 0.5, order_pos.z))
 		end
+	elseif not selected_something then
+		player_selected_objects = {}
 	end
 end
